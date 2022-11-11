2a34e1492dac7006b061b215cd1a12db7152493d
sadik182 <sadikurrahman728@gmail.com>
2022-11-11 22:29:34 +0600

    component done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
 
      <h2>Loading...</h2>
+     <Home></Home>
     </div>
   );
 }
